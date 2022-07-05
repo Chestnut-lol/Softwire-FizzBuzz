fa84cd7d0655014f195391877363ff4cd488c733
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-05 14:25:04 +0100

    Completed stretch goals part 1.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace FizzBuzz
+{
+    class Program
+    {
+        static string Is3or5(int i)
+        {
+            string res;
+            if ((i % 3 == 0) & (i % 5 == 0))
+            {
+                res = "FizzBuzz";
+            }
+            else if (i % 3 == 0)
+            {
+                res = "Fizz";
+            }
+            else //(i % 5 == 0)
+            {
+                res = "Buzz";
+            }
+            return res;
+        }
+        static string FizzBuzz(int i)
+        {
+            if ((i % 3 == 0) | (i % 5 == 0))
+            {
+                string ans = Is3or5(i);
+                if (i % 7 == 0)
+                {
+                    ans += "Bang";
+                    
+                }
+                return ans;
+            }
+            else
+            {
+                return i.ToString();
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+        }
+    }
+}
