5d1e3a69ee73f32ced95d0e0eb31e92d5f3dbdc2
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-05 14:44:39 +0100

    Completed all stretch goals.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,6 +4,12 @@ namespace FizzBuzz
 {
     class Program
     {
+        public static string Reverse( string s )
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse( charArray );
+            return new string( charArray );
+        }
         static string Is3or5(int i)
         {
             string res;
@@ -21,28 +27,68 @@ namespace FizzBuzz
             }
             return res;
         }
-        static string FizzBuzz(int i)
+
+        static string Is13(string ans)
         {
-            if ((i % 3 == 0) | (i % 5 == 0))
+            // Check if ans is numeric
+            if (int.TryParse(ans, out int _))
             {
-                string ans = Is3or5(i);
-                if (i % 7 == 0)
-                {
-                    ans += "Bang";
-                    
-                }
-                return ans;
+                return "Fezz";
+            }
+            int idx = ans.IndexOf('B');
+            if (idx == -1)
+            {
+                ans += "Fezz";
             }
             else
             {
-                return i.ToString();
-                
+                ans = ans.Insert(idx, "Fezz");
             }
+
+            return ans;
+            
+            
+        }
+        static string FizzBuzz(int i)
+        {
+            string ans;
+            if (i % 11 == 0)
+            {
+                ans = "Bong";
+            }
+            else if ((i % 3 == 0) | (i % 5 == 0))
+            {
+                ans = Is3or5(i);
+                if (i % 7 == 0)
+                {
+                    ans += "Bang";
+                }
+            }
+            else if (i % 7 == 0)
+            {
+                ans = "Bang";
+            }
+            else
+            {
+                ans = i.ToString();
+            }
+
+            if (i % 13 == 0)
+            {
+                ans = Is13(ans);
+            }
+
+            if (i % 17 == 0)
+            {
+                ans = Reverse(ans);
+            }
+
+            return ans;
         }
 
         static void Main(string[] args)
         {
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 200; i++)
             {
                 Console.WriteLine(FizzBuzz(i));
             }
